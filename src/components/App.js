db46c38efa68e415918a2eaa17cd0879f57fb94e
Justyna <justynasadlak10@gmail.com>
2019-05-25 04:24:42 +0200

    Create mood rating

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MoodRatio from './MoodRatio';
+import '../app.css';
+
+function App() {
+  return (
+    <div className="ui container">
+        <MoodRatio messageId="asd"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MoodRatio.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Rating from "react-rating";
+import '../app.css';
+
+export default class MoodRatio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { messageId: this.props.messageId, rate: 0, date: "" };
+    this.rating = React.createRef();
+  }
+
+  async onSubmitRating() {
+    await this.setState({ rate: this.rating.current.state.value, date: Date.now() });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className="rate-container">
+        <p>Oceń swoje samopoczucie:</p>
+        <Rating
+          ref={this.rating}
+          name="rate"
+        />
+        <br />
+        <button
+          className="ui positive button"
+          onClick={this.onSubmitRating.bind(this)}
+        >
+          Prześlij
+        </button>
+      </div>
+    );
+  }
+}
